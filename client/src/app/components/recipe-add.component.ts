7d47c933ed46163bbc3ab1d42f2be60411f98679
boxyboxy <eugenengboxiang@gmail.com>
2022-02-11 14:39:53 +0800

    finished form without POST method

--- client/src/app/components/recipe-add.component.ts
+++ client/src/app/components/recipe-add.component.ts
@@ -23,8 +23,10 @@ export class RecipeAddComponent implements OnInit {
 
   createForm(): FormGroup {
     return this.fb.group({
-      name: this.fb.control('', [Validators.required]),
-      ingredients: this.fb.array([new FormControl('')]),
+      name: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+      ingredients: this.fb.array([
+        new FormControl('', [Validators.required, Validators.minLength(3)]),
+      ]),
       instruction: this.fb.control('', [Validators.required]),
       image: this.fb.control('', [Validators.required]),
     });
@@ -32,11 +34,20 @@ export class RecipeAddComponent implements OnInit {
 
   // add ingredient to formArray
   onAddIngredient() {
-    const control = new FormControl('', Validators.required);
+    const control = new FormControl('', [
+      Validators.minLength(3),
+      Validators.required,
+    ]);
     (<FormArray>this.form.get('ingredients')).push(control);
   }
 
   returnFormArray(form: FormGroup, key: string): FormArray {
     return form.get(key) as FormArray;
   }
+
+  removeIngredientControl(i: number) {
+    (<FormArray>this.form.get('ingredients')).removeAt(i);
+  }
+
+  saveRecipe() {}
 }
